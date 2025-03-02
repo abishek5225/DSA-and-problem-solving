1d806c414a53fa8dd3c2fc121b762c550e5acd4a
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-02 23:33:58 +0545

    updated

--- js/problem11.js
+++ js/problem11.js
@@ -1,5 +1,7 @@
 //product of an array
 
+/*
+//by using for of loop
 function productOfArray(arr){
     let result=1;
     for(let num of arr){
@@ -8,3 +10,11 @@ function productOfArray(arr){
     return result   
 }
 console.log(productOfArray([1,2,3,4,5])) //120;
+
+*/
+
+//by using reduce method
+function productofarr(arr){
+    return arr.reduce((a,b)=>a*b)
+}
+console.log(productofarr([1,2,3,4,5])) //120;
