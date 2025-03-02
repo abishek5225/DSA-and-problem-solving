b42c0ddd6ef4d841614de19ee8b07678d7157332
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-02 23:22:56 +0545

    updated

--- js/problem9.js
+++ js/problem9.js
@@ -1,1 +1,9 @@
 //finding missing number in an array
+
+function missingNum(arr){
+    let n=arr.length+1;
+    let expectedSum= (n*(n+1)/2)
+    let actualSum = arr.reduce((a,b)=>a+b)
+    return expectedSum-actualSum
+}
+console.log(missingNum([1,2,3,4,5,6,7,9])) 
