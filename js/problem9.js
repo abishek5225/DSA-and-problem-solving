8c0e71c3e365c664d2838cafaf4e98e52970c203
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-02 23:18:05 +0545

    updated

--- js/problem9.js
+++ js/problem9.js
@@ -1,9 +1,1 @@
 //finding missing number in an array
-
-function missingNumber(arr){
-    let n =arr.length;
-    let expectedsum=(n*(n+1))/2;
-    let actualsum=arr.reduce((a,b)=>a+b,0);
-    return expectedsum-actualsum;
-}
-console.log(missingNumber([3,0,1])); 
