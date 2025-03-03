003750fed9293246f9ae8c698178a3a0c6605ab2
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-03 09:04:22 +0545

    updated

--- js/problem10.js
+++ js/problem10.js
@@ -1,5 +1,5 @@
-//finding intersection of two arrays
-
+//finding intersection of two arrays 
+/*
 function interSection(arr1,arr2){
     let set1=new Set(arr1);
     let result=[]
@@ -11,5 +11,15 @@ function interSection(arr1,arr2){
         }
     }
     return result
+}
+console.log(interSection([1,2,2,3,3,4],[2,3,3,4,5]));
+
+*/
+
+//using filter
+
+function interSection(arr1,arr2){
+    let set1=new Set(arr1);
+    return arr2.filter(num=>set1.has(num))
 }
 console.log(interSection([1,2,2,3,3,4],[2,3,3,4,5]));
