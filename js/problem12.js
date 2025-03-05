848d3e4baa29e525c6ee4328241284eda4622848
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-05 20:55:30 +0545

    updated

--- js/problem12.js
+++ js/problem12.js
@@ -1,4 +1,13 @@
-// const nums=[1,2,3,4,5,6,7,8,9,10];
+const nums=[1,2,3,4,5,6,7,8,9,10];
+let result=[];
+const evennums=nums.forEach((item)=>{
+    if(item%2==0){
+        result.push(item)
+    }
+    return result;
+})
+
 // const evennums = nums.filter((item)=>item%2==0)
-// console.log(evennums);
+
+console.log(result);
 
