a6376ad1e01702f582dc5ac1f5cf8ea5820eeb57
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-05 20:50:05 +0545

    updated

--- js/problem12.js
+++ js/problem12.js
@@ -1,3 +1,4 @@
-const nums=[1,2,3,4,5,6,7,8,9,10];
-const evennums = nums.filter((item)=>item%2==0)
-console.log(evennums);
+// const nums=[1,2,3,4,5,6,7,8,9,10];
+// const evennums = nums.filter((item)=>item%2==0)
+// console.log(evennums);
+
