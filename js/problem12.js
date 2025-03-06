1438d84a1ed858b33d023b01e0e2a98c8ef90087
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-06 18:40:21 +0545

    updated

--- js/problem12.js
+++ js/problem12.js
@@ -1,5 +1,5 @@
 //seeing filter
-
+/*
 const nums=[1,2,3,4,5,6,7,8,9,10];
 let result=[];
 const evennums=nums.forEach((item)=>{
@@ -12,3 +12,9 @@ const evennums=nums.forEach((item)=>{
 // const evennums = nums.filter((item)=>item%2==0)
 
 console.log(result)
+*/
+
+const books = [
+  { title: "Book1", genre: "fiction", publish: 1981, edition: 2004 },
+  { title: "Book2", genre: "non-fiction", publish: 1985, edition: 2005 },
+];
