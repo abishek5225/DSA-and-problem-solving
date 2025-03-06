2c507fc42512cb454949d211a38b2237d0b100b7
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-06 18:58:54 +0545

    updated

--- js/problem12.js
+++ js/problem12.js
@@ -17,4 +17,11 @@ console.log(result)
 const books = [
   { title: "Book1", genre: "fiction", publish: 1981, edition: 2004 },
   { title: "Book2", genre: "non-fiction", publish: 1985, edition: 2005 },
+  { title: "Book3", genre: "science", publish: 1999, edition: 2007 },
+  { title: "Book4", genre: "fiction", publish: 2000, edition: 2008 },
+  { title: "Book5", genre: "science", publish: 2005, edition: 2010 },
+  { title: "Book6", genre: "History", publish: 1887, edition: 1987 },
 ];
+
+const fictionBooks = books.filter((book) => book.genre === "fiction");
+console.log(fictionBooks);
