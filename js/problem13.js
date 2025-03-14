4938ce0656b6c21802162ae01b7fa4bbd380131a
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-14 21:12:09 +0545

    updated

--- js/problem13.js
+++ js/problem13.js
@@ -1,5 +1,13 @@
 function firstNonRepeatingChar(str){
     let charMap = new Map();
 
-    
+    for(let char of str){
+        charMap.set(char, (charMap.get(char) || 0)+1)
+    }
+    for(let char of str){
+        if(charMap.get(char) === 1){
+            return char
+        }
+    }
 }
+
