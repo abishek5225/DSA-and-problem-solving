6b051cf9899db1171ffce81317710dcf5dff739e
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-14 21:51:03 +0545

    updated

--- js/problem14.js
+++ js/problem14.js
@@ -16,6 +16,13 @@ myPromise.then(result =>{
 let myPromise = new Promise((resolve, reject)=>{
     setTimeout(()=>{
         let success = false
-        
-    })
+
+        if(success){
+            resolve("task completed")
+        }else{
+            reject("something went wrong")
+        }
+    },5000)
 })
+myPromise.then(result=> console.log(result))
+.catch(error => console.log("error", error))
