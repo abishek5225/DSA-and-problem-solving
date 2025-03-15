5f22398b6bdcc01c6c846c48d3aaf807dfa09ce5
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-15 19:58:44 +0545

    updated

--- js/problem15.js
+++ js/problem15.js
@@ -1,11 +1,29 @@
+//just a demo of async and await
+
+/*
 function delay(ms){
     return new Promise((resolve)=>{
         setTimeout(resolve,ms)
     })
 }
-
+// delay(ms) returns a Promise that resolves after ms milliseconds.
 async function example(){
     console.log('start')
-    await delay(3000)
-    
+    await delay(3000) //pauses execution for 3 seconds.
+    console.log("Ended successfully")
 }
+
+example()
+*/
+
+// fetching data from an API using async/await
+
+async function fetchData(){
+    try{
+        let response = await fetch('https://jsonplaceholder.typicode.com/posts')
+        let data = await response.json()
+        console.log(data)
+    }catch(error){
+        console.log("error fetching data:", error)
+    }
+}fetchData()
