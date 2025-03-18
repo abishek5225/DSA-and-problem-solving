d8aaa337e3f2e1251a283e0b002d5d51cf95292f
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-18 19:07:41 +0545

    updated

--- js/problem16.js
+++ js/problem16.js
@@ -1,4 +1,12 @@
 //Bubble sort
 function bubbleSort(arr){
-    
+    let n=arr.length;
+    let swapped;
+
+    for(let i =0;i<n-1;i++){
+        swapped = false
+        for(let j=0;j<n-1;i++){
+            
+        }
+    }
 }
