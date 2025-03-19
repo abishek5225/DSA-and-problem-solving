2b258d3690e7dbf8daa1d47c6506730fcec2149a
abishek_5225 <abishekchaulagain9815007991@gmail.com>
2025-03-19 23:51:32 +0545

    updated

--- js/problem17.js
+++ js/problem17.js
@@ -1,3 +1,9 @@
-console.log("helloworld")
-console.log("hellomars")
-console.log("hello world")
+function sortArray(arr){
+    let n=arr.length;
+    let swapped;
+
+    for(let i=0;i<n-1;i++){
+        swapped = false;
+        
+    }
+}
